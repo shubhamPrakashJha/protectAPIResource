f233643a83a7eb1c6e92f262b51fb2702fb6e2df
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-05-04 14:02:37 +0530

    Initial commit

---
+++ views.py
@@ -0,0 +1,52 @@
+from models import Base, User
+from flask import Flask, jsonify, request, url_for, abort, g
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
+
+engine = create_engine('sqlite:///users.db')
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+app = Flask(__name__)
+
+
+@app.route('/users', methods=['POST'])
+def new_user():
+	username = request.json.get('username')
+	password = request.json.get('password')
+	if username is None or password is None:
+		print "missing arguments"
+		abort(400)
+
+	if session.query(User).filter_by(username=username).first() is not None:
+		print "existing user"
+		user = session.query(User).filter_by(username=username).first()
+		return jsonify({
+			'message': 'user already exists'}), 200  # , {
+	# 'Location': url_for('get_user', id = user.id, _external = True)}
+
+	user = User(username=username)
+	user.hash_password(password)
+	session.add(user)
+	session.commit()
+	return jsonify({
+		'username': user.username}), 201
+
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+	user = session.query(User).filter_by(id=id).one()
+	if not user:
+		abort(400)
+	return jsonify({'username': user.username})
+
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host='0.0.0.0', port=5000)
